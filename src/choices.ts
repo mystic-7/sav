e1e3c2c68b365afbddbc7a647e0130e1a027df09
Gerardo Coronado <gcmarin9@gmail.com>
2023-05-29 10:53:30 -0400

    choices temp

---
+++ src/choices.ts
@@ -0,0 +1,7 @@
+const choices = [
+    { id: 1, name: 'opcion 1' },
+    { id: 2, name: 'opcion 2' },
+    { id: 3, name: 'opcion 3' },
+  ];
+
+  export default choices
--- src/components/ChoiceCardGrid.tsx
+++ src/components/ChoiceCardGrid.tsx
@@ -1,12 +1,8 @@
 import { SimpleGrid } from '@chakra-ui/react';
 import ChoiceCard from './ChoiceCard';
+import choices from '../choices';
 
 const GameGrid = () => {
-  const choices = [
-    { id: 1, name: 'opcion 1' },
-    { id: 2, name: 'opcion 2' },
-    { id: 2, name: 'opcion 2' },
-  ];
   return (
     <>
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding={15} spacing={10}>
