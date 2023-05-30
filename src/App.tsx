9f41bccd0c777cafbb22b9f28faf08c7ae51409f
Gerardo Coronado <gcmarin9@gmail.com>
2023-05-30 11:17:13 -0400

    colorsitos puestos como es debido

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
-import { Center, Grid, GridItem } from '@chakra-ui/react';
+import {
+  Center,
+  Grid,
+  GridItem,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import Logo from './components/Logo';
 import Stage from './components/Stage';
+import './App.css';
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const background = useColorModeValue('light.gradient', 'dark.gradient');
+
+  // Function to check if it's currently day time
+  const isDayTime = () => {
+    const currentHour = new Date().getHours();
+    return currentHour >= 6 && currentHour < 18;
+  };
+
+  // Update the color mode based on the time of day
+  if (
+    (colorMode === 'light' && !isDayTime()) ||
+    (colorMode === 'dark' && isDayTime())
+  ) {
+    toggleColorMode();
+  }
+
   return (
     <Grid
       className="body"
+      bg={background}
+      backgroundSize="400% 400%"
+      animation="gradient 15s ease infinite"
+      height="100vh"
       templateAreas={{
         base: ` 'nav' 'main'`,
       }}
       h="100vh"
-      justifyItems={'center'}
+      justifyItems="center"
     >
-      <GridItem area="nav" w={'100%'}>
+      <GridItem area="nav" w="100%">
         <Center>
           <Logo />
         </Center>
       </GridItem>
-      <GridItem area="main" w={'100%'} placeItems="center">
+      <GridItem area="main" w="100%" placeItems="center">
         <Stage />
       </GridItem>
     </Grid>
--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,14 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
-const config: ThemeConfig = {
+const theme = extendTheme({
+  colors: {
+    light: {
+      gradient: "linear-gradient(-45deg, #F9D89C, #F9D4C1, #C7D2FE, #F5E1A4)",
+    },
+    dark: {
+      gradient: "linear-gradient(-45deg, #3D3F5B, #4F4F6B, #6B6E8D, #0F0F0F)",
+    },
+  },
+});
 
-}
-
-const theme = extendTheme({config})
-
-export default theme
+export default theme;
