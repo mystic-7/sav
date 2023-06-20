1635d9b46b9cd6d77374eb969c0b0de9a4de2ebe
Gerardo Coronado <gcmarin9@gmail.com>
2023-06-20 09:43:59 -0400

    animations

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -1,22 +1,24 @@
-import { Box, Center, Collapse, Fade, Grid, GridItem } from '@chakra-ui/react';
+import { Box, Center, Grid, GridItem } from '@chakra-ui/react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import CardGrid from '../CardGrid';
 import './Stage.css';
 import ChatBox from '../ChatBox/ChatBox';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 const Stage = () => {
   const [showCardGrid, setShowCardGrid] = useState(false);
-  const showBar = true;
   const handleRequest = (text: string) => {
     console.log(text);
     setShowCardGrid(true);
   };
+  const [gridRef] = useAutoAnimate();
 
   return (
     <Box w={'100%'}>
       <Grid
+        ref={gridRef}
         templateColumns="1fr"
         templateRows={showCardGrid ? '1fr' : 'auto 1fr'}
         gap={4}
@@ -37,16 +39,24 @@ const Stage = () => {
               <SearchBar onRequest={handleRequest} />
             </motion.div>
           </AnimatePresence>
-          {!showCardGrid && <Box h={100} />}
+          {!showCardGrid && <Box h={130} />}
         </GridItem>
-        <GridItem>
-          <Collapse in={showCardGrid}>
-            <Center>
-              <ChatBox />
-            </Center>
-            <CardGrid />
-          </Collapse>
-        </GridItem>
+        {showCardGrid && (
+          <GridItem>
+            <motion.div
+              key="card-grid"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.5, ease: 'easeOut' }}
+            >
+              <Center>
+                <ChatBox />
+              </Center>
+              <CardGrid />
+            </motion.div>
+          </GridItem>
+        )}
       </Grid>
     </Box>
   );
